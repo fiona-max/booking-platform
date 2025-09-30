37360b112c15e1de2b9b0b11cf1f9e790d0b7f45
fiona-max <anyefiona@gmail.com>
2025-09-30 17:24:45 +0100

    feat: added user authentication and form validation

--- src/app/features/auth/login/login.ts
+++ src/app/features/auth/login/login.ts
@@ -1,25 +1,42 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import {FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators} from '@angular/forms';
 import {AuthService} from '../../../core/services/auth/auth-service';
+import {RouterLink} from '@angular/router';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [CommonModule, FormsModule],
+  imports: [CommonModule, FormsModule, ReactiveFormsModule, RouterLink],
   templateUrl: './login.html',
   styleUrl: './login.scss'
 })
 export class Login {
-  email = '';
-  password = '';
+  loginForm: FormGroup;
 
-  constructor(private auth: AuthService) {}
+  constructor(private auth: AuthService, private fb: FormBuilder) {
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]]
+    });
+  }
+  get email() {
+    return this.loginForm.get('email');
+  }
+
+  get password() {
+    return this.loginForm.get('password');
+  }
 
   onLogin() {
-    this.auth.login(this.email, this.password).subscribe({
-      next: user => console.log('Logged in:', user),
-      error: err => console.error(err)
-    });
+    if (this.loginForm.invalid) {
+      this.loginForm.markAllAsTouched(); // show errors
+      return;
+    }else {
+      this.auth.login(this.loginForm.value.email, this.loginForm.value.password).subscribe({
+        next: user => console.log('Logged in:', user),
+        error: err => console.error(err)
+      });
+    }
   }
 }
--- src/app/features/auth/register/register.ts
+++ src/app/features/auth/register/register.ts
@@ -1,39 +1,71 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import {AbstractControl, FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators} from '@angular/forms';
 import { AuthService } from '../../../core/services/auth/auth-service';
+import {RouterLink} from '@angular/router';
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [CommonModule, FormsModule],
+  imports: [CommonModule, FormsModule, ReactiveFormsModule, RouterLink],
   templateUrl: './register.html',
   styleUrl: './register.scss'
 })
 export class Register {
-  email = '';
-  password = '';
+  signupForm: FormGroup;
 
-  constructor(private auth: AuthService) {}
-  // toggleDarkMode(): void {
-  //   this.isDarkMode = !this.isDarkMode;
-  //   if (this.isDarkMode) {
-  //     this.renderer.addClass(document.body, 'dark');
-  //     localStorage.setItem('theme', 'dark');
-  //   } else {
-  //     this.renderer.removeClass(document.body, 'dark');
-  //     localStorage.setItem('theme', 'light');
-  //   }
-  // }
+  showPassword = false;
+  showConfirmPassword = false;
 
-  onSubmit(form: any) {
-    if (form.valid) {
-      console.log('Form submitted', form.value);
-    }
+  constructor(private auth: AuthService, private fb: FormBuilder) {
+    this.signupForm = this.fb.group(
+      {
+        fullName: ['', [Validators.required, Validators.minLength(3)]],
+        email: ['', [Validators.required, Validators.email]],
+        password: ['', [Validators.required, Validators.minLength(6)]],
+        confirmPassword: ['', Validators.required]
+      },
+      { validators: this.passwordsMatchValidator }
+    );
   }
-  onRegister() {
-    this.auth.register(this.email, this.password).subscribe({
-      next: user => console.log('Registered:', user),
-      error: err => console.error(err)
-    });
+
+  get fullName() {
+    return this.signupForm.get('fullName');
+  }
+  get email() {
+    return this.signupForm.get('email');
+  }
+  get password() {
+    return this.signupForm.get('password');
+  }
+  get confirmPassword() {
+    return this.signupForm.get('confirmPassword');
+  }
+
+  passwordsMatchValidator(control: AbstractControl) {
+    const password = control.get('password')?.value;
+    const confirmPassword = control.get('confirmPassword')?.value;
+    if (password !== confirmPassword) {
+      control.get('confirmPassword')?.setErrors({ mismatch: true });
+    } else {
+      control.get('confirmPassword')?.setErrors(null);
+    }
+    return null;
+  }
+
+  togglePasswordVisibility(field: 'password' | 'confirm') {
+    if (field === 'password') this.showPassword = !this.showPassword;
+    else this.showConfirmPassword = !this.showConfirmPassword;
+  }
+
+  onRegister(form: any) {
+    if (this.signupForm.invalid) {
+      return;
+    }else{
+      this.auth.register(form.value.email, form.value.password).subscribe({
+        next: user => console.log('Registered:', user),
+        error: err => console.error(err)
+      });
+    }
+
   }
 }
